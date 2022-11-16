a454f1bb5fc05095b4bd6d5ca52597f2af161cdf
Matthew Smilansky <msmilansky@gmail.com>
2022-11-16 14:47:28 -0800

    CHORE Added Tailwind, planning folder files, several images, and first divs in Home page.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+import Navbar from '../src/components/ui/Navbar'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  )
 }
