59cc201a6e4c88ef93c0d84664a3cde42adb3564
Matthew Smilansky <msmilansky@gmail.com>
2023-01-14 23:57:26 -0800

    CHORE: More refactoring of Order page.

--- pages/order/index.tsx
+++ pages/order/index.tsx
@@ -4,20 +4,23 @@ import { useRouter } from 'next/router'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 import { ChevronDownIcon, FunnelIcon, MinusIcon, PlusIcon, Squares2X2Icon } from '@heroicons/react/20/solid'
 import useBreakpoint from '@utils/use-breakpoint'
-import { sortOptions, subCategories, products, filterOptions } from '@data/order'
+import { sortOptions, subCategories, products } from '@data/order'
 import ProductFiltersMobile from '@products/ProductFiltersMobile'
 import ProductGrid from '@products/ProductGrid'
+import ProductsMenu from '@products/ProductsMenu'
+import ProductFilters from '@products/ProductFilters'
 
 function classNames(...classes) {
-	return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(' ')
 }
 
 function AllProductsPage() {
-	const router = useRouter()
+  const router = useRouter()
 
-	console.log(router.query)
-	const { breakpoint, windowSize } = useBreakpoint()
-
+  console.log(router.query)
+  const { breakpoint, windowSize } = useBreakpoint()
+  {
+    /** 
 	const handleCheckbox = e => {
 		setFilters(prevState => {
 			return {
@@ -40,141 +43,32 @@ function AllProductsPage() {
 			}
 		}
 	}
+*/
+  }
+  return (
+    <div className='bg-white'>
+      <div>
+        {/* Mobile filters dialog */}
+        {/* The 'show' prop controls whether the children should be shown or hidden */}
+        <ProductFiltersMobile />
 
-	return (
-		<div className='bg-white'>
-			<div>
-				{/* Mobile filters dialog */}
-				{/* The 'show' prop controls whether the children should be shown or hidden */}
-				<ProductFiltersMobile />
-
-				<main className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-					<div className='flex items-baseline justify-between border-b border-gray-200 pt-24 pb-6'>
-						<h1 className='text-4xl font-bold tracking-tight text-gray-900'>Psst... the cookies are good.</h1>
-
-						<div className='flex items-center'>
-							<Menu as='div' className='relative inline-block text-left'>
-								<div>
-									<Menu.Button className='group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900'>
-										Sort
-										<ChevronDownIcon
-											className='-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500'
-											aria-hidden='true'
-										/>
-									</Menu.Button>
-								</div>
-
-								<Transition
-									as={Fragment}
-									enter='transition ease-out duration-100'
-									enterFrom='transform opacity-0 scale-95'
-									enterTo='transform opacity-100 scale-100'
-									leave='transition ease-in duration-75'
-									leaveFrom='transform opacity-100 scale-100'
-									leaveTo='transform opacity-0 scale-95'
-								>
-									<Menu.Items className='absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none'>
-										<div className='py-1'>
-											{sortOptions.map(option => (
-												<Menu.Item key={option.name}>
-													{({ active }) => (
-														<a
-															href={option.href}
-															className={classNames(
-																option.current ? 'font-medium text-gray-900' : 'text-gray-500',
-																active ? 'bg-gray-100' : '',
-																'block px-4 py-2 text-sm'
-															)}
-														>
-															{option.name}
-														</a>
-													)}
-												</Menu.Item>
-											))}
-										</div>
-									</Menu.Items>
-								</Transition>
-							</Menu>
-
-							<button type='button' className='-m-2 ml-5 p-2 text-gray-400 hover:text-gray-500 sm:ml-7'>
-								<span className='sr-only'>View grid</span>
-								<Squares2X2Icon className='h-5 w-5' aria-hidden='true' />
-							</button>
-							<button
-								type='button'
-								className='-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden'
-								onClick={() => setMobileFiltersOpen(true)}
-							>
-								<span className='sr-only'>Filters</span>
-								<FunnelIcon className='h-5 w-5' aria-hidden='true' />
-							</button>
-						</div>
-					</div>
-
-					<section aria-labelledby='products-heading' className='pt-6 pb-24'>
-						<h2 id='products-heading' className='sr-only'>
-							Products
-						</h2>
-						{/*********************************************************************************/}
-						<div className='grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4'>
-							{/* Filters */}
-							<form className='hidden lg:block'>
-								<Disclosure as='div' className='border-b border-gray-200 py-6'>
-									{({ open, close }) => (
-										<>
-											{open && windowSize.width < 960 && close()}
-											<h3 className='-my-3 flow-root'>
-												<Disclosure.Button className='flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500'>
-													{/* FILTER TYPE NAME */}
-													<span className='font-medium text-gray-900'>Categories</span>
-													<span className='ml-6 flex items-center'>
-														{open ? (
-															<MinusIcon className='h-5 w-5' aria-hidden='true' />
-														) : (
-															<PlusIcon className='h-5 w-5' aria-hidden='true' />
-														)}
-													</span>
-												</Disclosure.Button>
-											</h3>
-											<Transition
-												enter='transition duration-100 ease-out'
-												enterFrom='transform scale-95 opacity-0'
-												enterTo='transform scale-100 opacity-100'
-												leave='transition duration-75 ease-out'
-												leaveFrom='transform scale-100 opacity-100'
-												leaveTo='transform scale-95 opacity-0'
-											>
-												<Disclosure.Panel className='pt-6'>
-													<div className='space-y-4'>
-														<div className='flex items-center'>
-															{/* FILTER INPUT */}
-															<input
-																id={`filter-${2}-${2}`}
-																name='sup'
-																defaultValue={true}
-																onChange={e => handleCheckbox(e)}
-																type='checkbox'
-																defaultChecked={true}
-																className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
-															/>
-															<label htmlFor={`filter-${2}-${2}`} className='ml-3 text-sm text-gray-600'>
-																'pies'
-															</label>
-														</div>
-													</div>
-												</Disclosure.Panel>
-											</Transition>
-										</>
-									)}
-								</Disclosure>
-							</form>
-							<ProductGrid />
-						</div>
-					</section>
-				</main>
-			</div>
-		</div>
-	)
+        <main className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+          <ProductsMenu />
+          <section aria-labelledby='products-heading' className='pt-6 pb-24'>
+            <h2 id='products-heading' className='sr-only'>
+              Products
+            </h2>
+            {/*********************************************************************************/}
+            <div className='grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4'>
+              {/* Filters */}
+              <ProductFilters />
+              <ProductGrid />
+            </div>
+          </section>
+        </main>
+      </div>
+    </div>
+  )
 }
 
 export default AllProductsPage
--- src/components/Products/ProductGrid.tsx
+++ src/components/Products/ProductGrid.tsx
@@ -1,9 +1,10 @@
 import { Fragment, useState, useCallback, useEffect } from 'react'
 import { Dialog, Disclosure, Menu, Transition } from '@headlessui/react'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 import { ChevronDownIcon, FunnelIcon, MinusIcon, PlusIcon, Squares2X2Icon } from '@heroicons/react/20/solid'
 import useBreakpoint from '@utils/use-breakpoint'
-import { sortOptions, subCategories, products, filterOptions } from '@data/order'
+import { sortOptions, subCategories } from '@data/order'
 import ProductFiltersMobile from '@products/ProductFiltersMobile'
 
 const ProductGrid = () => {
@@ -16,10 +17,11 @@ const ProductGrid = () => {
 					<div className='mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
 						<h2 className='sr-only'>Products</h2>
 						<div className='grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
+							{/*
 							{products.map(product => (
 								<a key={product.id} href={product.href} className='group animate-[fadeIn_1s_linear] transition-all'>
 									<div className='aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8'>
-										<img
+										<Image
 											src={product.imageSrc}
 											alt={product.imageAlt}
 											className='h-full w-full object-cover object-center group-hover:opacity-75'
@@ -29,6 +31,7 @@ const ProductGrid = () => {
 									<p className='mt-1 text-lg font-medium text-gray-900'>{product.price}</p>
 								</a>
 							))}
+							*/}
 						</div>
 					</div>
 				</div>
